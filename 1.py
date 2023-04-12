2c74e1e4124b2ef99075f1dcb4488d52acb11660
amin-rabiee <121285967+amin-rabiee@users.noreply.github.com>
2023-04-13 00:00:45 +0330

    Delete 1.py

--- 1.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://admin:admin@127.0.0.1')
-db = client['mflix']
-movie_COLLECTION = db['movies']
-
-
-for i in movie_COLLECTION.find({'genres':'History'}, {'title':1}):
-    print(i)
